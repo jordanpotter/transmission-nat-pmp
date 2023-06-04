7f1c89aca5295d1fdf10d06c4aaab573a4e26208
Jordan Potter <me@jordanpotter.com>
2023-06-03 21:12:39 -0700

    Implement NAT-PMP logic

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/jordanpotter/transmission-nat-pmp
 
 go 1.20
+
+require github.com/jackpal/go-nat-pmp v1.0.2
--- main.go
+++ main.go
@@ -1,9 +1,53 @@
 package main
 
 import (
-	"fmt"
+	"flag"
+	"log"
+	"net"
+	"time"
+
+	natpmp "github.com/jackpal/go-nat-pmp"
 )
 
+const (
+	portMappingRequestBuffer = 15 * time.Second
+)
+
+var (
+	gatewayIPStr string
+)
+
+func init() {
+	flag.StringVar(&gatewayIPStr, "gateway", "", "gateway ip address")
+	flag.Parse()
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	gatewayIP := net.ParseIP(gatewayIPStr)
+	if gatewayIP == nil {
+		log.Fatalf("failed to parse gateway ip address: %s", gatewayIPStr)
+	}
+
+	natpmpClient := natpmp.NewClient(gatewayIP)
+
+	previousExternalPort := uint16(0)
+
+	for {
+		portMapping, err := natpmpClient.AddPortMapping("tcp", 0, int(previousExternalPort), 600)
+		if err != nil {
+			log.Fatalf("failed to add port mapping: %v", err)
+		}
+
+		nextPortMappingTime := time.Now().Add(time.Duration(portMapping.PortMappingLifetimeInSeconds)*time.Second - portMappingRequestBuffer)
+
+		if portMapping.MappedExternalPort != previousExternalPort {
+			log.Printf("external port changed to %d", portMapping.MappedExternalPort)
+
+			previousExternalPort = portMapping.MappedExternalPort
+
+			// TODO: update Transmission
+		}
+
+		time.Sleep(time.Until(nextPortMappingTime))
+	}
 }
